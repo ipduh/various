85f0f4c92b7680d1f14d278f589bb2e924976a91
George Paitaris <wo@ipduh.com>
2018-12-12 20:11:55 +0200

    clean up

--- devz/mdnator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Create README.md for devz
-# g0 2014 - http://ipduh.com/contact
-# http://sl.ipduh.com/devz-howto
-
-. ./devz.sh
-devz |awk 1 ORS='<br />\n'|sed 's/devz<br \/>/devz/g'|sed 's/=<br \/>/ /g' |sed 's/*/ /g' > ./README.md
